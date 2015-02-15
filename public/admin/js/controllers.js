e51f877d0ac8e89b22bb008f79d1cbb0e24ea8a4
A-gambit <a.gambit.gregory@gmail.com>
2015-02-15 18:09:55 +0200

    make admin

--- public/admin/js/controllers.js
+++ public/admin/js/controllers.js
@@ -4,7 +4,37 @@ var adminControllers = angular.module('adminControllers', []);
 
 adminControllers.controller('mainCtrl', ['$scope',
     function ($scope) {
-        $scope.user = "Alex";
+        $scope.type = "user";
     }]);
 
+adminControllers.controller('navCtrl', ['$scope',
+    function ($scope) {
+        $scope.navModelItems = [
+            {
+                data: 'user',
+                text: 'Users',
+                active: true
+            },
+            {
+                data: 'company',
+                text: 'Companies',
+                active: false
+            },
+            {
+                data: 'post',
+                text: 'Posts',
+                active: false
+            }
+        ];
+
+        $scope.index = 0;
+
+        $scope.changeModel = function ($event, $index){
+            var model = $scope.navModelItems[$index];
+            $scope.type = model.data;
+            $scope.navModelItems[$scope.index].active = false;
+            $scope.navModelItems[$index].active = true;
+            $scope.index = $index;
+        }
+    }]);
 
--- public/admin/js/directives.js
+++ public/admin/js/directives.js
@@ -8,3 +8,19 @@ adminDirectives.directive("header", function () {
         templateUrl: "partials/header.html"
     };
 });
+
+adminDirectives.directive("selectmodel", function () {
+    return {
+        restrict: 'E',
+        templateUrl: "partials/select-models.html",
+        controller: 'navCtrl'
+    };
+});
+
+adminDirectives.directive("modellist", function () {
+    return {
+        restrict: 'E',
+        templateUrl: "partials/model-list.html",
+        controller: 'mainCtrl'
+    };
+});
