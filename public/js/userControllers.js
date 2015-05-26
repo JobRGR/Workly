b794d03fa0931f46e585e64ca98f605a71f4b742
Ignat <iob.veritas@gmail.com>
2015-05-26 18:32:47 +0300

    user page date

--- public/js/userControllers.js
+++ public/js/userControllers.js
@@ -3,4 +3,21 @@ var userControllers = angular.module('userControllers', []);
 userControllers.controller('UserCtrl',['$scope', '$http', 'AuthService',
     function($scope, $http, AuthService){
 
+
+        $scope.getDateFormat = function (date) {
+            var newDate = new Date(date);
+            return strDate(newDate);
+        };
+
+        function strDate(date){
+            var isCurMonth = date.getUTCFullYear() == (new Date()).getUTCFullYear()
+                    && date.getUTCMonth() == (new Date()).getUTCMonth()
+                , isCurDay = date.getDate() == (new Date).getDate()
+                , isYesterday = date.getDate() == (new Date).getDate() - 1;
+
+            if (!isCurMonth) return date.toLocaleDateString();
+            if (isCurDay) return 'Cьогодні '+ date.toLocaleTimeString();
+            if (isYesterday) return 'Вчора '+date.toLocaleTimeString();
+            return date.toLocaleDateString();
+        }
     }]);
