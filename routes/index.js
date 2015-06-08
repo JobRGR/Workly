ca4359029d8d6c3a6e92c4a6e39493fe386fe1c8
A-gambit <a.gambit.gregory@gmail.com>
2015-06-09 00:20:59 +0300

    feed

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ module.exports = function(app) {
     var GetModels = require('./get-models')(app);
     var Render = require('./render')(app);
     var Post = require('./post')(app);
-    var Subscribe = require('./subscribe')(app);
     var Respond = require('./respond')(app);
     var Search = require('./search')(app);
 };
